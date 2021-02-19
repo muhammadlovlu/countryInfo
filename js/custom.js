d4249f3b353d0f3ab224dc1137840573bb39f328
muhammadlovlu <chyshahab@gmail.com>
2021-02-19 16:19:02 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,19 @@
 fetch("https://restcountries.eu/rest/v2/all")
     .then(res => res.json())
     .then(data => showCountries(data))
-    
+
 const showCountries = countries => {
     const output = document.getElementById("allCountries");
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i];
-
+    countries.forEach(country => {
         const createDiv = document.createElement("div");
         createDiv.className = 'country';
         const countryInfo = `
             <h3 class="countryName">${country.name}</h3>
             <p class="capital">${country.capital}</p>
-        `
+        `;
         createDiv.innerHTML = countryInfo;
-        // const h3 = document.createElement("h3");
-        // h3.innerText = country.name;
-        // createDiv.appendChild(h3);
-
-        // const p = document.createElement("p");
-        // p.innerText = country.capital;
-        // createDiv.appendChild(p);
         output.appendChild(createDiv);
-    }
+    });
 }
 
 
